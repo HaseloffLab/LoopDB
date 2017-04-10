05a94ee76727537f4c3b57d24c660741ac400df0
Mihails Delmans <md656@cam.ac.uk>
2017-04-10 18:08:03 +0100

    Adding gb to schema-example

--- schema-example/schema.py
+++ schema-example/schema.py
@@ -3,7 +3,9 @@ from loopDB import LoopDB
 loopDB = LoopDB( 'postgresql:///loopdb', clean = True)
 loopDB.initFromFile('schema.json')
 
-cds = loopDB.addPart(name = "my CDS", backbone = "L0-CDS", seq = "ATGTAA")
-loopDB.commit()
+prom5_35s = loopDB.addPart(name = '35s Promoter', 	gbFile = 'gb/35S_PROM5.ape', backbone = 'L0-Prom5')
+cds_eGFP  = loopDB.addPart(name = 'eGFP',			gbFile = 'gb/eGFP_CDS.ape',  backbone = 'L0-CDS-')
+tag_lti	  = loopDB.addPart(name = 'LTI Tag',		gbFile = 'gb/Lti6b-TAG.ape', backbone = 'L0-CTAG')
+term_35s  = loopDB.addPart(name = '35s Terminator', gbFile = 'gb/35S_TERM.ape',  backbone = 'L0-TERM3')
 
-print "{0}: {1}".format( cds.dbid, cds.name )
+loopDB.commit()
