8ba9211f73f39cb83d07d70055ec6036508c299d
Bernardo Pollak <bpollakw@gmail.com>
2017-07-11 13:45:49 +0100

    heroku

--- server.py
+++ server.py
@@ -209,4 +209,3 @@ if __name__ == '__main__':
 	loopDB.initFromFile('schema.json')
 	port = int(os.environ.get('PORT', 5000))	
 	socketio.run(app, debug=True,host='0.0.0.0', port = port)
-	f __name__ == "__main__":
