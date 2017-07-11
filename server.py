ccd2af11a1f9cb68984732174d4217228028ce68
Bernardo Pollak <bpollakw@gmail.com>
2017-07-11 14:16:58 +0100

    heroku

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ app.secret_key = 'SECRET'
 
 socketio = SocketIO(app)
 
-loopDB = LoopDB( 'postgresql:///loopdb')
+loopDB = LoopDB( 'postgres://yvzfsazuhpepmv:3b1e1b321abd1c7d7851c53ee0adf25d0fa701203aa1d00def8ed8ab3deef3f8@ec2-54-83-25-217.compute-1.amazonaws.com:5432/df2063nflr5ueh')
 
 def w2uiFormToDict(request):
 	return json.loads(request.form.to_dict()['request'])["record"]
