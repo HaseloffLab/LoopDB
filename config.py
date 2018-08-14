311bd94d65d2b698fc1b5e46f0ddbe4bd4b7bdce
Bernardo Pollak <bpollakw@gmail.com>
2018-08-13 18:38:22 -0700

    herokuap

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 DEBUG			= True
-SECRET_KEY		= ""
+SECRET_KEY		= "secret"
 DATABASE_URL	= "postgres://yvzfsazuhpepmv:3b1e1b321abd1c7d7851c53ee0adf25d0fa701203aa1d00def8ed8ab3deef3f8@ec2-54-83-25-217.compute-1.amazonaws.com:5432/df2063nflr5ueh"
 SCHEMA_PATH		= "./schema2.json"
